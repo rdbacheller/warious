b6f66375575eec51457af3c14d5af8c3bb76728f
rdbacheller <rd.bacheller@gmail.com>
2015-04-22 12:06:38 -0400

    Attempting fix 0.0

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,10 +2,10 @@ class LandingController < ApplicationController
 
   def index
     #posts = Post.order('created_at DESC');
-    @postOne = '1';#posts[0];
-    @postTwo = '2';#posts[1];
-    @postThree = '3';#posts[2];
-    @postFour = '4';#posts[3];
+    #@postOne = '1';#posts[0];
+    #@postTwo = '2';#posts[1];
+    #@postThree = '3';#posts[2];
+    #@postFour = '4';#posts[3];
   end
 
   def pad

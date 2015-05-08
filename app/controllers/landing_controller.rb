e41ff6d959047dbaab531a8cc70b0e946abc09fe
rdbacheller <rd.bacheller@gmail.com>
2015-05-07 22:15:29 -0400

    Testing with database entry

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,8 +1,8 @@
 class LandingController < ApplicationController
 
   def index
-    #posts = Post.order('created_at DESC');
-    #@postOne = '1';#posts[0];
+    posts = Post.order('created_at DESC');
+    @postOne = posts[0];
     #@postTwo = '2';#posts[1];
     #@postThree = '3';#posts[2];
     #@postFour = '4';#posts[3];

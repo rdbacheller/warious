cc14cbf8b0e8fae33ff69c686e0bd199190fbbd4
rdbacheller <rd.bacheller@gmail.com>
2015-05-07 23:52:37 -0400

    Test 0.1

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -3,9 +3,9 @@ class LandingController < ApplicationController
   def index
     posts = Post.order('created_at DESC');
     @postOne = posts[0];
-    #@postTwo = '2';#posts[1];
-    #@postThree = '3';#posts[2];
-    #@postFour = '4';#posts[3];
+    @postTwo = posts[1];
+    @postThree = posts[2];
+    @postFour = posts[3];
   end
 
   def pad
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Post.create(title: 'Test', blurb: 'A test blurb. . .', body: 'A test body. . .', picture: 'some/test/path')
+Post.create(title: 'Another Test', blurb: 'Another test blurb. . .', body: 'Another test body. . .', picture: 'some/other/test/path')

d2c54eb24c9f3d2282e27cff75d252898d2627ef
rdbacheller <rd.bacheller@gmail.com>
2015-05-25 22:00:11 -0400

    Added comments and EXTREMELY basic error-handling in case of missing posts or an empty database

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,14 +1,34 @@
 class LandingController < ApplicationController
 
   def index
-    # Needs to implement error-handling and content checking/validation
+    # content checking/validation
 
-    #posts = Post.order('created_at DESC');
+    # Pulls all of the posts from the Post table
+    posts = Post.order('created_at DESC');
 
-    #@postOne = posts[0];
-    #@postTwo = posts[1];
-    #@postThree = posts[2];
-    #@postFour = posts[3];
+    # Creates a temporary blank post
+    blank = Post.new(title: 'Non-Existent Article',blurb: 'This is filler text',body: 'This is filler for a non-existent article', picture: 'tile.jpg', url: 'nonexistent');
+    # Initializes the instance variables to be blank by default, this comes in handy for error-checking
+    @postOne = blank
+    @postTwo = blank
+    @postThree = blank
+    @postFour = blank
+
+    # Tests to see if the entry is blank (nil), if it isn't then the proper post is set; this works on a post-by-post basis,
+    # and is ugly but highly effective
+    if !posts[0].nil?
+      if !posts[1].nil?
+        if !posts[2].nil?
+          if !posts[3].nil?
+            @postFour = posts[3]
+          end
+          @postThree = posts[2]
+        end
+        @postTwo = posts[1]
+      end
+      @postOne = posts[0]
+      return
+    end
   end
 
   def pad
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-Post.create(title: 'Test', blurb: 'A test blurb. . .', body: 'A test body. . .', picture: 'some/test/path')
+Post.create(title: 'Test', blurb: 'A test blurb. . .', body: 'A test body. . .', picture: 'tile.jpg')
 Post.create(title: 'Another Test', blurb: 'Another test blurb. . .', body: 'Another test body. . .', picture: 'some/other/test/path')

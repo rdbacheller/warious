3c1c5d2805911b5950a7ef50ac132e66ab884250
rdbacheller <rd.bacheller@gmail.com>
2015-05-28 19:39:15 -0400

    Added some migrations to the database, as well as re-added the seed file, temporarily

---
+++ db/migrate/20150527222321_create_series.rb
@@ -0,0 +1,16 @@
+class CreateSeries < ActiveRecord::Migration
+  def up
+    create_table :series do |t|
+      t.column "title", :string, :limit => 128, :default => "", :null => false
+      t.column "author", :string, :limit => 32, :default => "Emmanuel Goldstein", :null => false
+      t.column "total", :int, :default => 0, :null => false
+      t.column "meta", :string, :default => "", :null => false
+
+      t.timestamps null: false
+    end
+  end
+
+  def down
+    drop_table :series
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-#Post.create(title: 'Test', blurb: 'A test blurb. . .', body: 'A test body. . .', picture: 'tile.jpg')
-#Post.create(title: 'Another Test', blurb: 'Another test blurb. . .', body: 'Another test body. . .', picture: 'some/other/test/path')
+Post.create(
+ title: 'Test',
+ blurb: 'A test blurb. . .',
+ body: 'A test body. . .',
+ picture: 'tile.jpg')
+Post.create(
+ title: 'Another Test',
+ blurb: 'Another test blurb. . .',
+ body: 'Another test body. . .',
+ picture: 'some/other/test/path')
